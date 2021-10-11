13c2ac2cd408fd8b3211f3ac6401dec281323c0e
Luciano de Paula <luciano.desenvolvedor@gmail.com>
2021-10-11 16:44:56 -0300

    Exemplo de uso dos verbos

--- src/main/java/br/com/rest/RestApiApplication.java
+++ src/main/java/br/com/rest/RestApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiPerson {
+public class RestApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestApiPerson.class, args);
+		SpringApplication.run(RestApiApplication.class, args);
 	}
 
 }
